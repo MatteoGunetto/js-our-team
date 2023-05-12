df7aff664ddae8b8673b60898760605245cab7de
MatteoGunetto <Matteogunetto@tiscali.it>
2023-05-12 17:00:33 +0200

    milestone1/2

--- team.js
+++ team.js
@@ -7,5 +7,54 @@
 // BONUS 1:
 // Trasformare la stringa foto in una immagine effettiva
 // BONUS 2: Organizzare i singoli membri in card/schede
+const arrMembers = [
+	{
+		name: 'Wayne Barnett',
+		role: 'Founder & CEO',
+		img: 'wayne-barnett-founder-ceo.jpg',
+	},
+	{
+		name: 'Angela Caroll',
+		role: 'Chief Editor',
+		img: 'angela-caroll-chief-editor.jpg',
+	},
+	{
+		name: 'Walter Gordon',
+		role: 'Office Manager',
+		img: 'walter-gordon-office-manager.jpg',
+	},
+	{
+		name: 'Angela Lopez',
+		role: 'Social Media Manager',
+		img: 'angela-lopez-social-media-manager.jpg',
+	},
+	{
+		name: 'Scott Estrada',
+		role: 'Developer',
+		img: 'scott-estrada-developer.jpg',
+	},
+	{
+		name: 'Barbara Ramos',
+		role: 'Graphic Designer',
+		img: 'barbara-ramos-graphic-designer.jpg',
+	},
+];
+let person
+for (let i= 0 ; i < arrMembers.length; i++){
+    let member = arrMembers[i]
+    console.log(member);
+
+    for (const key in member) {
+        person = (member [key])
+        console.log(person);
+
+        document.getElementById('person').innerHTML += person; 
+        
+        
+    }
+    
+    
+}
+
 
 
